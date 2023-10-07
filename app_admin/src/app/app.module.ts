a8ac866c8c1422556d4aa084629f79b74ffabc88
glovernm <nicholasmglover@outlook.com>
2023-10-07 01:18:26 -0700

    Implemented the Admin SPA with Angular

---
+++ app_admin/src/app/app.module.ts
@@ -0,0 +1,37 @@
+// Modules
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-router.module';
+
+// Components
+import { AppComponent } from './app.component';
+import { TripListingComponent } from './trip-listing/trip-listing.component';
+import { TripCardComponent } from './trip-card/trip-card.component';
+import { AddTripComponent } from './add-trip/add-trip.component';
+import { EditTripComponent } from './edit-trip/edit-trip.component';
+
+// Services
+import { TripDataService } from '../../services/trip-data.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TripListingComponent,
+    TripCardComponent,
+    AddTripComponent,
+    EditTripComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [
+    TripDataService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
